4a4443c2f3ad9036a2f7d8e66833d3d2ffb81c6c
SeungHo <sh0win9907@gmail.com>
2025-02-16 21:08:01 +0900

    1. ObjectPool.h에 Raw 통합

--- GameServerEngine/ServerCommon_LIB/Utility/private/Job/JobPool/JobPool.cpp
+++ GameServerEngine/ServerCommon_LIB/Utility/private/Job/JobPool/JobPool.cpp
@@ -1,4 +1,4 @@
-﻿#include <pch.h>
+#include <pch.h>
 #include <Job/JobPool/JobPool.h>
 #include <Job/Job.h>
 
@@ -6,6 +6,6 @@ namespace sh::Utility {
 
 template <typename T, typename... Args>
 JobPtr JobPool::GetJobObject(std::function<void(Args...)> jobFunc, std::shared_ptr<T>& owner, Args... args) {
-  return ObjectPool<JobPtr>::MakeShared(std::move(Job::GenerateCaller(jobFunc, owner, std::forward<Args>(args)...)));
+  return MakeShared(std::move(Job::GenerateCaller(jobFunc, owner, std::forward<Args>(args)...)));
 }
 }  // namespace sh::Utility
--- GameServerEngine/ServerCommon_LIB/Utility/public/SingletonBase/Singleton.h
+++ GameServerEngine/ServerCommon_LIB/Utility/public/SingletonBase/Singleton.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 namespace sh::Utility {
 template <class T>
