972271ea60550e61f3416c106790fe850e4e5414
SeungHo <sh0win9907@gmail.com>
2025-03-28 05:34:26 +0900

    1. 매칭 클래스 수정

--- DreamWorld_Server/Match/Matching.h
+++ DreamWorld_Server/Match/Matching.h
@@ -1,37 +1,31 @@
-//#pragma once
-//#include "../SingletonBase.h"
-//#include "MatchQueue.h"
-//
-//class UserSession;
-//class Matching : public SingletonBase<Matching>
-//{
-//	friend SingletonBase;
-//private:
-//	Matching() = default;
-//	~Matching()
-//	{
-//		spdlog::info("Matching::~Matching()");
-//	}
-//public:
-//	void InserMatch(std::shared_ptr<UserSession>& userRef, const ROLE& role);
-//	void CancelMatch(std::shared_ptr<UserSession>& userRef, const ROLE& role);
-//
-//	void StartMatching();
-//private:
-//	void MatchFunc();
-//
-//	std::shared_ptr<UserSession> GetCurrentMatchUserSession(MatchQueue& matchQueue, std::shared_ptr<UserSession> lastUserSession);
-//
-//private:
-//	MatchQueue m_warriorQueue;
-//	MatchQueue m_archerQueue;
-//	MatchQueue m_mageQueue;
-//	MatchQueue m_tankerQueue;
-//
-//	std::weak_ptr<UserSession> m_lastWarriorUser;
-//	std::weak_ptr<UserSession> m_lastMageUser;
-//	std::weak_ptr<UserSession> m_lastTankerUser;
-//	std::weak_ptr<UserSession> m_lastArcherUser;
-//
-//};
-//
+#pragma once
+#include "../SingletonBase.h"
+#include "MatchQueue.h"
+#include "../DreamWorldDefine.h"
+
+namespace DreamWorld {
+class Session;
+class Matching : public SingletonBase<Matching> {
+ public:
+  void InsertMatch(std::shared_ptr<Session>& userRef, const ROLE role);
+  void CancelMatch(std::shared_ptr<Session>& userRef, const ROLE role);
+
+  void StartMatching();
+
+ private:
+  void MatchFunc();
+
+  std::shared_ptr<Session> GetCurrentMatchUserSession(MatchQueue& matchQueue, std::shared_ptr<Session> lastUserSession);
+
+ private:
+  MatchQueue m_warriorQueue;
+  MatchQueue m_archerQueue;
+  MatchQueue m_mageQueue;
+  MatchQueue m_tankerQueue;
+
+  std::weak_ptr<Session> m_lastWarriorUser;
+  std::weak_ptr<Session> m_lastMageUser;
+  std::weak_ptr<Session> m_lastTankerUser;
+  std::weak_ptr<Session> m_lastArcherUser;
+};
+}  // namespace DreamWorld
