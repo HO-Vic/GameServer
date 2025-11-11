d32b4e76d769cd7f5f410bcce6e62614858bf4e4
SeungHo <sh0win9907@gmail.com>
2025-11-11 20:58:54 +0900

    주석 수정

--- SimpleTCPEcho/Client/main.cpp
+++ SimpleTCPEcho/Client/main.cpp
@@ -67,6 +67,7 @@ int main() {
   ReadParseMsg(parsedMsg);
 
   while (true) {
+    // 단어 가변 배열 전송
     auto packetSize = parsedMsg.GetSerializeSize();
     auto sendBuffer = sh::IO_Engine::SendBufferAllocator::GetInstance().GetShared(packetSize);
     auto writePtr = sendBuffer->GetWritePtr(packetSize);
@@ -75,6 +76,7 @@ int main() {
 
     Sleep(100);
 
+    // 가변 문자열들 전송
     for (const auto& msgData : datas) {
       // WRITE_LOG(SimpleTCP::LogLevel::info, "Send [PayLoadSize: {}] Send Msg: {}", msgData.GetPayloadMsg().size(), msgData.GetPayloadMsg());
       auto packetSize = msgData.GetSerializeSize();
@@ -92,10 +94,4 @@ int main() {
   }
   Sleep(1000);
   WRITE_LOG(SimpleTCP::LogLevel::info, "All Msg Send");
-  // 1. 단순 문자열 전송, 문자열은 각기 다른 길이를 가짐
-  // 2. 문자열에 대해서 단어별 파싱(스페이스 기준)이걸 통으로 보내기, 즉, 어떤 메모리 layout으로 동작해야할지
-  // 3. 모든 문자열을 한번에 전송시켜버리기
-  // serialize/deserialize에 대한 공부
-  /*for (const auto& data : datas) {
-  }*/
 }
